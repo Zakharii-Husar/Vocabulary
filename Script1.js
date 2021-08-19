84860878391dd6541c5b4e7d148c0736e12f2ade
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 23:14:53 +0100

    df

--- Script1.js
+++ Script1.js
@@ -1,15 +1,14 @@
 import { english, ukrainian, transcription } from "./words.js";
 
 let favourite = [];
-const like = document.getElementById("like");
-like.addEventListener("click", function () { favourite.push("kek") });
-localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 let data = localStorage.getItem("favouriteWords");
 let favouriteWords = JSON.parse(data);
 
+const like = document.getElementById("like");
+like.addEventListener("click", function () { favourite.push("kek") });
+localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 
 console.log(favouriteWords);
-console.log(favourite);
 
 const know = [];
 const doubt = [];
