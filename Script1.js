a558b2639980090360389731e1f1b8ecebefa4c7
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 23:16:42 +0100

    sdf

--- Script1.js
+++ Script1.js
@@ -1,12 +1,14 @@
 import { english, ukrainian, transcription } from "./words.js";
 
 let favourite = [];
+if (favourite.length > 0) {
+    localStorage.setItem("favouriteWords", JSON.stringify(favourite));
+}
 let data = localStorage.getItem("favouriteWords");
 let favouriteWords = JSON.parse(data);
 
 const like = document.getElementById("like");
 like.addEventListener("click", function () { favourite.push("kek") });
-localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 
 console.log(favouriteWords);
 
