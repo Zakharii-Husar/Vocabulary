f73de36f2760e0c0d4507e782691aa3e88b515e3
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 23:11:59 +0100

    dfd

--- Script1.js
+++ Script1.js
@@ -2,7 +2,7 @@ import { english, ukrainian, transcription } from "./words.js";
 
 const favourite = [];
 const like = document.getElementById("like");
-like.addEventListener("click", function pushing () { favourite.push("wss") });
+like.addEventListener("click", function () { console.log("wewe") });
 localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 let data = localStorage.getItem("favouriteWords");
 let favouriteWords = JSON.parse(data);
