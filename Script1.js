a2ed88591173c1872c385f97319bc71c92691fd8
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 22:01:08 +0100

    dd

--- Script1.js
+++ Script1.js
@@ -1,11 +1,10 @@
 import { english, ukrainian, transcription } from "./words.js";
 
-const favourite = [];
-localStorage.setItem("favouriteWords", JSON.stringify(favourite));
-let favouriteWords = JSON.parse(localStorage.getItem("favouriteWords"));
+const favourite2 = ["wew"];
+localStorage.setItem("favouriteWords2", JSON.stringify(favourite2));
+let favouriteWords = JSON.parse(localStorage.getItem("favouriteWords2"));
 
-const like = document.getElementById("like");
-like.addEventListener("click", favourite.push("wowo"))
+
 
 console.log(favouriteWords[0]);
 const know = [];
