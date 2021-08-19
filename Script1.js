4ccc12bc10069b722b1861cfe6a1c06711d9e760
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 22:21:23 +0100

    df

--- Script1.js
+++ Script1.js
@@ -5,9 +5,10 @@ localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 let favouriteWords = JSON.parse(localStorage.getItem("favouriteWords"));
 
 const like = document.getElementById("like");
-like.addEventListener("click", favourite.push("wowo"))
+like.addEventListener("click", pushing = () => { favourite.push("wowo") });
 
 console.log(favouriteWords[0]);
+
 const know = [];
 const doubt = [];
 const dontKnow = [];
