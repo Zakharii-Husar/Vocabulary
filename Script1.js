bce89b6a67f99d056da7852ef667cfa4936cd44b
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 22:46:48 +0100

    dfd

--- Script1.js
+++ Script1.js
@@ -2,13 +2,14 @@ import { english, ukrainian, transcription } from "./words.js";
 
 const favourite = [];
 localStorage.setItem("favouriteWords", JSON.stringify(favourite));
-let favouriteWords = JSON.parse(localStorage.getItem("favouriteWords"));
+let data = localStorage.getItem("favouriteWords");
+let favouriteWords = JSON.parse(data);
 
 const like = document.getElementById("like");
 like.addEventListener("click", function () { favourite.push("wss") });
 
 console.log(favouriteWords);
-console.log(favourite[0] + "2");
+console.log(favourite[0] + "1");
 
 const know = [];
 const doubt = [];
