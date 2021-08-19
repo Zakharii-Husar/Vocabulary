48d33f0f3228a679f410f9086af8485c29afbe42
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 22:22:32 +0100

    dsd

--- Script1.js
+++ Script1.js
@@ -5,7 +5,7 @@ localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 let favouriteWords = JSON.parse(localStorage.getItem("favouriteWords"));
 
 const like = document.getElementById("like");
-like.addEventListener("click", pushing = () => { favourite.push("wowo") });
+like.addEventListener("click", function () { favourite.push("wowo") });
 
 console.log(favouriteWords[0]);
 
