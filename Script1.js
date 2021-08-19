cb269b47379683f513d4dd08554ef4ef9b62bb7d
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 22:36:58 +0100

    sd

--- Script1.js
+++ Script1.js
@@ -4,10 +4,10 @@ const favourite = ["wow"];
 localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 let favouriteWords = JSON.parse(localStorage.getItem("favouriteWords"));
 
-favourite.push("wowo")
-
 const like = document.getElementById("like");
-like.addEventListener("click", function () { console.log(favourite[0]); });
+like.addEventListener("click", function () { favourite.push("wss") });
+
+console.log(favourite);
 
 const know = [];
 const doubt = [];
