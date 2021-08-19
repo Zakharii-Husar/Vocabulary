9a867916b0008342ba7ad4045ba9429fd5665596
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-19 23:05:35 +0100

    erte

--- Script1.js
+++ Script1.js
@@ -1,12 +1,12 @@
 import { english, ukrainian, transcription } from "./words.js";
 
 const favourite = [];
-window.localStorage.setItem("favouriteWords", JSON.stringify(favourite));
-let data = window.localStorage.getItem("favouriteWords");
-let favouriteWords = JSON.parse(data);
-
 const like = document.getElementById("like");
 like.addEventListener("click", function () { favourite.push("wss") });
+localStorage.setItem("favouriteWords", JSON.stringify(favourite));
+let data = localStorage.getItem("favouriteWords");
+let favouriteWords = JSON.parse(data);
+
 
 console.log(favouriteWords);
 console.log(favourite);
