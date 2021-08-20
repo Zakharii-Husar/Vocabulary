629bf948a29e17a0e19501aab7d56cd18b60a2be
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-20 12:53:38 +0100

    erfe

--- Script1.js
+++ Script1.js
@@ -5,10 +5,9 @@ let favourite = ["first"];
 const like = document.getElementById("like");
 like.addEventListener("click", function () {
     favourite.push("second");
+    localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 });
 
-
-localStorage.setItem("favouriteWords", JSON.stringify(favourite));
 
 let data = localStorage.getItem("favouriteWords");
 let favouriteWords = JSON.parse(data);
