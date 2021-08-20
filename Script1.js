0cb69ae626511b869d7270fdb4ef6e0a2d90b71b
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-20 15:12:10 +0100

    dgdf

--- Script1.js
+++ Script1.js
@@ -6,12 +6,23 @@ if (localStorage.getItem("favouriteWords") !== null) {
     inputFavourite = outputFavourite;
 };
 
+const star = () => {
+    let starPic = document.getElementById("like");
+    if (!inputFavourite.includes(english[7])) {
+        starPic.setAttribute("src", "img/like.png");
+        inputFavourite.push(english[7]);
+    } else {
+        starPic.setAttribute("src", "img/unlike.png")
+        let i = inputFavourite.indexOf(english[7])
+        if (i > -1) {
+            inputFavourite.splice(i, 1)
+        }
+    }
+    localStorage.setItem("favouriteWords", JSON.stringify(inputFavourite));
+}
 
 const like = document.getElementById("like");
-like.addEventListener("click", function () {
-    inputFavourite.push(english[7]);
-    localStorage.setItem("favouriteWords", JSON.stringify(inputFavourite));
-});
+like.addEventListener("click", star);
 
 console.log(inputFavourite);
 
