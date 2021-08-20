9b2229fd088dca1d1b1ccaf258ac9a31b44e0738
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-20 15:39:07 +0100

    rfg

--- Script1.js
+++ Script1.js
@@ -1,23 +1,29 @@
 import { english, ukrainian, transcription } from "./words.js";
 
+let currentArray = english;
 let inputFavourite = [];
 let outputFavourite = JSON.parse(localStorage.getItem("favouriteWords"));
 if (localStorage.getItem("favouriteWords") !== null) {
     inputFavourite = outputFavourite;
 };
 
+const randomNum = () => {
+    Math.floor(Math.random() * currentArray.length)
+};
+
 const star = () => {
     let starPic = document.getElementById("like");
-    if (!inputFavourite.includes(english[7])) {
+    if (!inputFavourite.includes(english[randomNum()])) {
         starPic.setAttribute("src", "img/like.png");
-        inputFavourite.push(english[7]);
+        inputFavourite.push(english[randomNum()]);
     } else {
         starPic.setAttribute("src", "img/unlike.png")
-        let i = inputFavourite.indexOf(english[7])
+        let i = inputFavourite.indexOf(english[randomNum()])
         if (i > -1) {
             inputFavourite.splice(i, 1)
         }
     }
+
     localStorage.setItem("favouriteWords", JSON.stringify(inputFavourite));
 }
 
