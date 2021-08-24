7a054fbbbe8c057fed39393740fa9758d15dea42
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-24 12:53:01 +0100

    df

--- Script1.js
+++ Script1.js
@@ -12,11 +12,11 @@ let random = Math.floor(Math.random() * currentArrayEng.length);
 
 const textContent = () => {
     const word = document.getElementById("word");
-    word.textContent = currentArrayEng[0];
+    word.textContent = currentArrayEng[random];
     const transcription = document.getElementById("transcription");
-    transcription.textContent = currentTranscription[0];
+    transcription.textContent = currentTranscription[random];
     const translation = document.getElementById("flipside");
-    translation.textContent = currentArrayUa[0];
+    translation.textContent = currentArrayUa[random];
 };
 
 // FAVOURITE WORDS
