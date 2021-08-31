e2997f1dcdb3e5e5d728d630f5325fe890a1b1da
Zakharii <Zakharii@DESKTOP-1GTRJF1>
2021-08-31 01:06:43 +0100

    df

--- Script1.js
+++ Script1.js
@@ -227,11 +227,11 @@ flipBtn.addEventListener("click", () => {
 // MENU BUTTONS
         const buttonEffect = (btn, arr, color) => {
             btn.addEventListener("click", () => {
-                btn.style.boxShadow = "10px 5px 5px black";
-                btn.style.padding = "6%";
+                btn.style.boxShadow = "10px 5px 5px yellow";
+                //btn.style.padding = "6%";
                 setTimeout(() => {
                     btn.style.boxShadow = "none";
-                    btn.style.padding = "5%";
+                    //btn.style.padding = "5%";
                     HeaderColor = color;
                     currentArray = arr;
                     showCard();
