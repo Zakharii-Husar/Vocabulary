32069fa136e60c7c5f136226f2ca55b400524e54
Zakharii-Husar <zakhariihusar@gmail.com>
2022-06-11 16:51:04 +0300

    fixing a bug adding animation only on a second click

--- Script1.js
+++ Script1.js
@@ -2,165 +2,168 @@
 import textContent from './modules/textContent.js';
 
 //ARRAYS WITH INDEXES TO ACCES ARRAYS FROM JSON
-const allKeys = {
+const ALLKEYS = {
     knownWordsArr: [],
     doubtfulWordsArr: [],
     unknownWordsArr: Array.from(Array(1000).keys()),
     likedWordsArr: []
 };
 //GETTING DATA FROM LOCAL STORAGE FROM PREVIOUS SESSION IF PRESENT
-getLocalData(allKeys);
+getLocalData(ALLKEYS);
 
-let currentArray = allKeys.unknownWordsArr;
-let index = 0;
+let CURRENTARRAY = ALLKEYS.unknownWordsArr;
+let INDEX = 0;
+let HEADERCOLOR = 'red';
 
-    let HEADERCOLOR = 'red';
+const id = (id) => {
+    return document.getElementById(id)
+};
 
-    const id = (id) => {
-        return document.getElementById(id)
+const update = () => {
+    if (CURRENTARRAY) INDEX = CURRENTARRAY[0];
+
+    const star = () => {
+        if (ALLKEYS.likedWordsArr.includes(INDEX)) {
+            id("like").setAttribute("src", "img/like.png");
+        } else {
+            id("like").setAttribute("src", "img/unlike.png");
+        }
     };
-    
-    const update = () => {
-        if (currentArray) index = currentArray[0];
-        const star = () => {
-            if (allKeys.likedWordsArr.includes(index)) {
-                id("like").setAttribute("src", "img/like.png");
-            } else {
-                id("like").setAttribute("src", "img/unlike.png");
-            }
-        };
-        star();
-        id("menuHead").style.backgroundColor = HEADERCOLOR;
-        textContent(currentArray, index, allKeys);
+
+    star();
+
+    id("menuHead").style.backgroundColor = HEADERCOLOR;
+
+    textContent(CURRENTARRAY, INDEX, ALLKEYS);
+}
+update();
+
+document.addEventListener("click", update);
+
+const actions = (moveTo, swipeDirection) => {
+
+    const toggleLike = () => {
+        if (!moveTo.includes(INDEX)) {
+            moveTo.push(INDEX);
+        }
+        else {
+            ALLKEYS.likedWordsArr.splice(ALLKEYS.likedWordsArr.indexOf(INDEX), 1);
+        }
+    };
+
+    const move = () => {
+        if (CURRENTARRAY !== moveTo &&
+            CURRENTARRAY !== ALLKEYS.likedWordsArr) {
+            CURRENTARRAY.splice(CURRENTARRAY.indexOf(INDEX), 1);
+            moveTo.push(INDEX);
+        }
     }
-    update();
 
-    document.addEventListener("click", update);
+    const swipe = () => {
+        if (swipeDirection == "right") {
+            const firstElement = CURRENTARRAY.shift();
+            CURRENTARRAY.push(firstElement);
+        }
+        if (swipeDirection == "left") {
+            const lastElement = CURRENTARRAY.pop();
+            CURRENTARRAY.unshift(lastElement);
+        }
+    };
 
-    const actions = (moveTo, swipeDirection) => {
+    if (INDEX > -1) {
+        if (moveTo == ALLKEYS.likedWordsArr && !swipeDirection) {
+            toggleLike();
+        }
+        else {
+            move();
+            swipe();
+        }
+    }
 
-        const toggleLike = () => {
-            if (!moveTo.includes(index)) {
-                moveTo.push(index);
-            }
-            else {
-                allKeys.likedWordsArr.splice(allKeys.likedWordsArr.indexOf(index), 1);
-            }
-        };
+    //SAVING EDITED ARRAYS TO LOCAL STORAGE
+    setLocalData(ALLKEYS);
 
-        const move = () => {
-            if (currentArray !== moveTo &&
-                currentArray !== allKeys.likedWordsArr) {
-                currentArray.splice(currentArray.indexOf(index), 1);
-                moveTo.push(index);
-            }
+};
+
+const swipeCardAnimation = (btn, swipeDirection, arr) => {
+    id(btn).addEventListener("click", () => {
+        if (arr !== CURRENTARRAY) {
+            actions(CURRENTARRAY, swipeDirection);
+            id("card").classList.add("swipe" + swipeDirection);
+            setTimeout(() => { id("card").classList.remove("swipe" + swipeDirection); }, 1000)
         }
 
-        const swipe = () => {
-            if (swipeDirection == "right") {
-                const firstElement = currentArray.shift();
-                currentArray.push(firstElement);
-            }
-            if (swipeDirection == "left") {
-                const lastElement = currentArray.pop();
-                currentArray.unshift(lastElement);
-            }
-        };
+    });
 
-        if (index > -1) {
-            if (moveTo == allKeys.likedWordsArr && !swipeDirection) {
-                toggleLike();
-            }
-            else {
-                move();
-                swipe();
-            }
-        }
+}
 
-        //SAVING EDITED ARRAYS TO LOCAL STORAGE
-        setLocalData(allKeys);
+swipeCardAnimation("arrowRight", "right");
+swipeCardAnimation("arrowLeft", "left");
 
+const moveWord = (btn, arr) => {
+    swipeCardAnimation(btn, "down", arr);
+    id(btn).addEventListener("click", () => {
+        actions(arr, "down");
+    });
+};
+moveWord("know", ALLKEYS.knownWordsArr);
+moveWord("doubt", ALLKEYS.doubtfulWordsArr);
+moveWord("dontKnow", ALLKEYS.unknownWordsArr);
+
+const like = () => {
+    id("like").addEventListener("click", () => {
+        actions(ALLKEYS.likedWordsArr);
+    });
+}
+like();
+
+const flip = () => {
+    id("flip").addEventListener("click", () => {
+        id("card").classList.toggle("flip");
+        setTimeout(() => { id("transcription").classList.toggle("active"); }, 150)
+    });
+};
+flip();
+
+const changeVisibility = (obj, visibility) => {
+    obj.style.display = visibility;
+};
+
+const showMenu = () => {
+    id("menuBtn").addEventListener("click", () => {
+        CURRENTARRAY = null;
+        HEADERCOLOR = "darkblue";
+        changeVisibility(card, "none");
+        changeVisibility(id("topPanel"), "none");
+        changeVisibility(id("panel"), "none");
+        changeVisibility(id("menu"), "flex");
+        changeVisibility(id("arrows"), "none");
+    });
+};
+showMenu();
+
+const switchList = (btn, arr, color) => {
+    const hideMenu = () => {
+        changeVisibility(card, "flex");
+        changeVisibility(id("topPanel"), "flex");
+        changeVisibility(id("panel"), "flex");
+        changeVisibility(id("menu"), "none");
+        changeVisibility(id("arrows"), "flex");
     };
 
-    const swipeCardAnimation = (btn, swipeDirection, arr) => {
-        id(btn).addEventListener("click", () => {
-            if(arr !== currentArray){
-                actions(currentArray, swipeDirection);
-                id("card").classList.add("swipe" + swipeDirection);
-                setTimeout(() => { id("card").classList.remove("swipe" + swipeDirection); }, 1000)
-            }
+    btn.addEventListener("click", () => {
+        btn.style.boxShadow = "10px 5px 5px Highlight";
+        setTimeout(() => {
+            btn.style.boxShadow = "none";
+            HEADERCOLOR = color;
+            hideMenu();
+            CURRENTARRAY = arr;
+            update();
+        }, 300)
+    })
+};
 
-        });
-
-    }
-
-    swipeCardAnimation("arrowRight", "right");
-    swipeCardAnimation("arrowLeft", "left");
-
-    const moveWord = (btn, arr) => {
-        id(btn).addEventListener("click", () => {
-            swipeCardAnimation(btn, "down", arr);
-            actions(arr, "down");
-        });
-    };
-    moveWord("know", allKeys.knownWordsArr);
-    moveWord("doubt", allKeys.doubtfulWordsArr);
-    moveWord("dontKnow", allKeys.unknownWordsArr);
-
-    const like = () => {
-        id("like").addEventListener("click", () => {
-            actions(allKeys.likedWordsArr);
-        });
-    }
-    like();
-
-    const flip = () => {
-        id("flip").addEventListener("click", () => {
-            id("card").classList.toggle("flip");
-            setTimeout(() => { id("transcription").classList.toggle("active"); }, 150)
-        });
-    };
-    flip();
-
-    const changeVisibility = (obj, visibility) => {
-        obj.style.display = visibility;
-    };
-
-    const showMenu = () => {
-        id("menuBtn").addEventListener("click", () => {
-            currentArray = null;
-            HEADERCOLOR = "darkblue";
-            changeVisibility(card, "none");
-            changeVisibility(id("topPanel"), "none");
-            changeVisibility(id("panel"), "none");
-            changeVisibility(id("menu"), "flex");
-            changeVisibility(id("arrows"), "none");
-        });
-    };
-    showMenu();
-
-    const switchList = (btn, arr, color) => {
-        const hideMenu = () => {
-            changeVisibility(card, "flex");
-            changeVisibility(id("topPanel"), "flex");
-            changeVisibility(id("panel"), "flex");
-            changeVisibility(id("menu"), "none");
-            changeVisibility(id("arrows"), "flex");
-        };
-
-        btn.addEventListener("click", () => {
-            btn.style.boxShadow = "10px 5px 5px Highlight";
-            setTimeout(() => {
-                btn.style.boxShadow = "none";
-                HEADERCOLOR = color;
-                hideMenu();
-                currentArray = arr;
-                update();
-            }, 300)
-        })
-    };
-
-    switchList(id("unknownList"), allKeys.unknownWordsArr, "red");
-    switchList(id("doubtfulList"), allKeys.doubtfulWordsArr, "yellow");
-    switchList(id("knownList"), allKeys.knownWordsArr, "green");
-    switchList(id("favouriteList"), allKeys.likedWordsArr, "darkgoldenrod");
+switchList(id("unknownList"), ALLKEYS.unknownWordsArr, "red");
+switchList(id("doubtfulList"), ALLKEYS.doubtfulWordsArr, "yellow");
+switchList(id("knownList"), ALLKEYS.knownWordsArr, "green");
+switchList(id("favouriteList"), ALLKEYS.likedWordsArr, "darkgoldenrod");
